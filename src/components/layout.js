0bfb9c6ed61db8094c9566076d0a6e28bd75d56c
jins-library <jins4731@gmail.com>
2023-06-22 22:55:04 +0900

    pref: add Header component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,91 +1,20 @@
-import { Drawer, Grid, IconButton, Link } from "@mui/material"
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import HouseOutlinedIcon from '@mui/icons-material/HouseOutlined';
-import AssignmentIndOutlinedIcon from '@mui/icons-material/AssignmentIndOutlined';
-import CategoryIcon from '@mui/icons-material/Category';
-import DensityMediumOutlinedIcon from '@mui/icons-material/DensityMediumOutlined';
+import { Link } from "@mui/material"
 
 import * as React from "react"
-import { useState } from "react";
-
+import Header from "./header";
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  const [open, setOpen] = useState(false);
   let header
 
   if (isRootPath) {
     header = (
-      <div>
-        <Grid>
-          <IconButton color="secondary" aria-label="add an alarm" onClick={() => setOpen(true)}>
-            <DensityMediumOutlinedIcon/>      
-          </IconButton>
-          <Drawer
-            anchor="left"
-            open={open}
-            onClose={() => setOpen(false)}
-          >
-            <Box width={'300px'}>
-              <nav aria-label="secondary mailbox folders">
-                <List>
-                  <ListItem disablePadding>
-                    <ListItemButton>
-                      <ListItemText primary="Trash" />
-                    </ListItemButton>
-                  </ListItem>
-                </List>
-              </nav>
-              <Divider />
-              <nav>
-                <List>
-                  <ListItem disablePadding>
-                    <ListItemButton>
-                      <ListItemIcon>
-                        <HouseOutlinedIcon fontSize="large"/>
-                      </ListItemIcon>
-                      <ListItemText primary="Home" />
-                    </ListItemButton>
-                  </ListItem>
-                  <ListItem disablePadding>
-                    <ListItemButton>
-                      <ListItemIcon>
-                        <AssignmentIndOutlinedIcon fontSize="large"/>
-                      </ListItemIcon>
-                      <ListItemText primary="About" />
-                    </ListItemButton>
-                  </ListItem>
-                  <ListItem disablePadding>
-                    <ListItemButton>
-                      <ListItemIcon>
-                        <CategoryIcon fontSize="large"/>
-                      </ListItemIcon>
-                      <ListItemText primary="Category" />
-                    </ListItemButton>
-                  </ListItem>
-                </List>
-              </nav>
-            </Box>
-          </Drawer>
-        </Grid>
-        <Grid></Grid>
-        <Grid></Grid>
-      </div>
+      <Header/>
     )
   } else {
     header = (
-      <div>
-        <Link className="header-link-home" to="/">
-          {title}
-        </Link>
-      </div>
+      <Header/>
     )
   }
 
